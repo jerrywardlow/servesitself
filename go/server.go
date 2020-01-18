5af7536257e60f2a533bf832835864d66494ae95
Jerry Wardlow <jerry@jerryoftheyear.com>
2020-01-17 20:20:10 -0800

    Basic logging

--- go/server.go
+++ go/server.go
@@ -3,12 +3,17 @@
 package main
 
 import (
+    "fmt"
     "net/http"
 )
 
 func main() {
+    fmt.Println("Server running...")
+
     http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
         http.ServeFile(res, req, "./server.go")
+        fmt.Printf("%s %s", req.Method, req.URL)
     })
+
     http.ListenAndServe(":8080", nil)
 }
