1476033bf29774c9660419c0476dcd12a83c800a
Jerry Wardlow <jerry@jerryoftheyear.com>
2020-01-21 00:24:58 +0000

    Include request protocol in logging

--- go/server.go
+++ go/server.go
@@ -12,7 +12,7 @@ func main() {
 
     http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
         http.ServeFile(res, req, "./server.go")
-        fmt.Printf("%s %s %s %s", req.RemoteAddr, req.Method, req.URL, req.Header.Get("User-Agent"))
+        fmt.Printf("%s \"%s %s %s\" \"%s\"\n", req.RemoteAddr, req.Method, req.URL, req.Proto, req.Header.Get("User-Agent"))
     })
 
     http.ListenAndServe(":8080", nil)
