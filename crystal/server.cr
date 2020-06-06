20dedd30098b80f37addfeff3e788cb4891621b2
Jerry Wardlow <jerry@jerryoftheyear.com>
2020-06-05 18:13:39 -0700

    Boilerplate HTTP server

--- crystal/server.cr
+++ crystal/server.cr
@@ -1,1 +1,12 @@
 # This is a basic web server written in Crystal which serves it's own source code
+
+require "http/server"
+
+server = HTTP::Server.new do |context|
+    context.response.content_type = "text/plain"
+    context.response.print "Placeholder"
+end
+
+address = server.bind_tcp "0.0.0.0", 8080
+puts "Server running on port 8080..."
+server.listen
